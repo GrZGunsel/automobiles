f3188858fb96a8392ba35f7b6191214d05da634b
kshitiz Moktan <kshitizlama51@gmail.com>
2021-11-29 22:06:54 +0545

    sad

--- src/components/Homepage/Contact/Contact.js
+++ src/components/Homepage/Contact/Contact.js
@@ -1,28 +1,37 @@
 import React from "react";
 import "./Contact.scss";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 const Contact = () => {
   return (
     <section>
       <div className="container">
-        <div className="about">
+        <div className="about flex ">
           <div className="leftcontainer">
             <h4>Contact</h4>
-            <p>Get in touch with us</p>
+            <h2>Get in touch with us</h2>
             <div className="contactdetails">
-              <div className="location">
-                <div className="icon"></div>
+              <div className="location flex align-center">
+                <div className="icon flex align-center justify-center">
+                  <HiOutlineLocationMarker size={18} />
+                </div>
                 <p>loremispm</p>
               </div>
-              <div className="Phonenumber">
-                <div className="icon"></div>
+              <div className="Phonenumber flex align-center">
+                <div className="icon flex align-center justify-center">
+                  <HiOutlineLocationMarker size={18} />
+                </div>
                 <p>loremispm</p>
               </div>
-              <div className="emailAddress">
-                <div className="icon"></div>
+              <div className="emailAddress flex align-center">
+                <div className="icon flex align-center justify-center">
+                  <HiOutlineLocationMarker size={18} />
+                </div>
                 <p>loremispm</p>
               </div>
-              <div className="openingTime">
-                <div className="icon"></div>
+              <div className="openingTime flex align-center">
+                <div className="icon flex align-center justify-center">
+                  <HiOutlineLocationMarker size={18} />
+                </div>
                 <p>loremispm</p>
               </div>
             </div>
@@ -31,19 +40,29 @@ const Contact = () => {
             <div className="form">
               <form action="">
                 <div className="firstname">
-                  <input type="text" />
+                  <input type="text" placeholder="First Name" />
                 </div>
                 <div className="lastname">
-                  <input type="text" />
+                  <input type="text" placeholder="Last Name" />
                 </div>
                 <div className="email">
-                  <input type="text" />
+                  <input type="text" placeholder="Email Address" />
                 </div>
                 <div className="number">
-                  <input type="text" />
+                  <input type="text" placeholder="Number" />
                 </div>
                 <div className="message">
-                  <input type="text" />
+                  <textarea
+                    name=""
+                    id=""
+                    rows="10"
+                    placeholder="Message"
+                  ></textarea>
+                </div>
+
+                <div className="sad">
+                  <p>asd</p>
+                  <html></html>
                 </div>
               </form>
             </div>
