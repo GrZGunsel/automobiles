f98878703384cdd1e348a1b62ef3386d7589221e
kshitiz Moktan <kshitizlama51@gmail.com>
2021-12-04 19:11:17 +0545

    sad

--- src/components/Homepage/Parts/Parts.js
+++ src/components/Homepage/Parts/Parts.js
@@ -3,7 +3,10 @@ import "./Part.scss";
 const Parts = ({ src, title, subtitle }) => {
   return (
     <div className="parts">
-      <div className="card" style={{ backgroundImage: `url(${src}})` }}>
+      <div className="card">
+        <div className="image">
+          <img src={src} alt="" />
+        </div>
         <div className="background-filter">
           <h4>{title}</h4>
           <p>{subtitle}</p>
